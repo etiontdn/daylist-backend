16f8996558f9717011b49a03e32dd3f0de7f76aa
João J <etiotndn@gmail.com>
2026-01-11 17:32:01 -0300

    Faz arquivos de models e enums.ts

---
+++ src/models/enums.ts
@@ -0,0 +1,20 @@
+export enum TipoUsuarioEnum {
+  CLIENTE = "CLIENTE",
+  ADMIN = "ADMIN"
+}
+
+export enum FrequenciaEnum {
+  DIARIO = "DIARIO",
+  SEMANAL = "SEMANAL"
+}
+
+export enum StatusHabitoEnum {
+  PENDENTE = "PENDENTE",
+  PARCIAL = "PARCIAL",
+  CONCLUIDO = "CONCLUIDO"
+}
+
+export enum SexoEnum {
+  M = "M",
+  F = "F"
+}
