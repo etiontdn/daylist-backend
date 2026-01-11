05c15b23c926cdc07831723573c0a5f4e2fea227
João J <etiotndn@gmail.com>
2026-01-10 22:48:15 -0300

    faz configuração básica de database usando mysql2

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from "express";
+import db from "./database";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('TypeScript with Express Server is running!');
+app.get("/", (req: Request, res: Response) => {
+    res.send("TypeScript with Express Server is running!");
+    db.query("SELECT * FROM EMPLOYEE;", (err, rows, fields) => {
+        if (err) throw err;
+        console.log(rows);
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
